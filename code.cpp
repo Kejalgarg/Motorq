bed2ede6e5a1e677bacb7d10cb1ef36245933459
Kejal Garg <145703317+Kejalgarg@users.noreply.github.com>
2025-08-05 17:29:02 +0530

    code.cpp

--- code.cpp
+++ code.cpp
@@ -1,6 +1,8 @@
 
 #include <iostream>
-#include <stdlib.h>
+#include <random>
+#include <string>
+#include<vector>
 using namespace std;
 
 int active=0;
@@ -46,40 +48,79 @@ class Vehicle{
 
         cout<<"Vehicle Identification Number: ";
         cin>>VIN;
-        cout<<endl;
   
         cout<<"Manufacturer: ";
         cin>>Manufacturer;
-        cout<<endl;
       
         cout<<"Model: ";
         cin>>Model;
-        cout<<endl;
       
-        cout<<"fleet type: ";
+        cout<<"fleet type(Corporate, Rental, Personal): ";
         cin>>fleetType;
-        cout<<endl;
 
         cout<<"Fleet ID: ";
         cin>>fleetId;
-        cout<<endl;
   
-        cout<<"Status: ";
+        cout<<"Status(Active, Inactive): ";
         cin>>status;
-        cout<<endl;
 
         cout<<"Owner: ";
         cin>>owner;
-        cout<<endl;
+
+        if (status == "Active") active++;
+        else if (status == "Inactive") inactive++;
 
     }
-/*
+
 //to generate dummy values of telementary data
     void telementaryData(){
 
+    random_device rd;
+    mt19937 gen(rd());
+
+    // Random generators
+    uniform_real_distribution<> latDist(-90.0, 90.0);
+    uniform_real_distribution<> lonDist(-180.0, 180.0);
+    uniform_int_distribution<> batteryDist(0, 100);
+    uniform_int_distribution<> speedDist(0, 200);
+    uniform_int_distribution<> odoDist(50, 300);
+    uniform_int_distribution<> timeDist(0, 100); 
+    uniform_int_distribution<> statusDist(0, 2); 
+
+
+    double latitude = latDist(gen);
+    double longitude = lonDist(gen);
+    int batteryPercentage = batteryDist(gen);
+    int speed = speedDist(gen);
+    int odometerReading = odoDist(gen);
+    long long timeStamp = timeDist(gen);
+    if(statusDist(gen)==0) string engineStatus="On";
+    if(statusDist(gen)==1) string engineStatus="Idle";
+    if(statusDist(gen)==2) string engineStatus="Off";
+
+    // Tracking stats
+    int active = 0, inactive = 0;
+    int battery = 0;
+    long long fleetDistance = 0;
+
+    battery += batteryPercentage;
+
+
+    fleetDistance += odometerReading;
+
+    //ALERT SYSTEM
+        //Alert for battery and high speed
+        if(battery<=15){
+            cout<<"ALERT: Low Battery"<<endl;
+        }
+
+        if(speed>50){
+            cout<<"ALERT: Over-speeding | speed>50Km/hr"<<endl;
+        }
+
 }
 
-*/
+/*
     //Telemenary data DUMMY VALUES through input
     void telementaryData(){
 
@@ -129,7 +170,7 @@ class Vehicle{
 
 }
 
-
+*/
 
     //To receive telementary data
     //to get the coordinates of that vehicle
@@ -208,13 +249,25 @@ int main(){
     Vehicle v1;
     Vehicle v2;
 
+    cout<<"Creating vehicle 1";
     v1.create(123, "Tesla", "Ev101", "personal", 321, "Active", "Kejal");
+    cout<<endl;
+
+    cout<<"Enter Info for Vehicle 2"<<endl;;
     v2.create();
+    cout<<endl;
 
+    cout<<"Vehicle 1 telementary data"<<endl;
     v1.telementaryData();
+    cout<<endl;
 
+    cout<<"Vehicle 1 general information"<<endl;
     v1.generalInfo();
+    cout<<endl;
+
+    cout<<"Vehicle 1 telementary info"<<endl;
     v1.telemenatryInfo();
+    cout<<endl;
 
 
     activeVehicles();
@@ -274,5 +327,50 @@ latitude: 20
 longitude: 120
 Odometer Reading: 150
 Speed: 80
+
+*/
+
+/*
+OUPUT using random values
+Creating vehicle 1
+Enter Info for Vehicle 2
+Vehicle Identification Number: 456
+Manufacturer: Honda
+Model: H984
+fleet type(Corporate, Rental, Personal): Rental
+Fleet ID: 654
+Status(Active, Inactive): Active
+Owner: Jiya
+
+Vehicle 1 telementary data
+
+Vehicle 1 general information
+Vehicle Identification Number: 123
+Manufacturer: Tesla
+Model: Ev101
+fleet type: personal
+Fleet ID: 321
+Status: Active
+Owner: Kejal
+
+Vehicle 1 telementary info
+Latitude: 0
+Longitude: 0
+Battery Level: 0
+Engine Status(On/Off/Idle): 
+Speed: 1826763056
+Odometer Reading: 1
+Time Stamp: 50288912
+
+Total Active Vehicles: 1
+Total Inactive Vehicles: 0
+Average Battery level: 0
+Total distance travelled by the fleet: 0
+Battery Percentage: 0
+Engine Status: 
+latitude: 0
+longitude: 0
+Odometer Reading: 1
+Speed: 1826763056
 
 */
